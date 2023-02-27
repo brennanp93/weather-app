f3010f2e9ef3f70ecbe60355241749a307e7b1fe
brennanp93 <brennan.p.perez@gmail.com>
2023-02-27 14:47:21 -0800

    API working. Add basic styling.

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   //   setLat(position.coords.latitude);
   //   setLong(position.coords.longitude);
   // });
+
   useEffect(() => {
     const fetchData = async () => {
       navigator.geolocation.getCurrentPosition(function (position) {
@@ -21,24 +22,27 @@ function App() {
         setLong(position.coords.longitude);
       });
 
-      await fetch(`${process.env.REACT_APP_BANANA}/weather/?lat=${lat}&lon=${long}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`)
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${33.7834494}&lon=${-117.8457956}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`)
         .then(res => res.json())
-        .then(result => {
-          setData(result)
-          console.log(long)
+        .then(res => {
+          setData(res)
+          // console.log(data)
+        })
+        .catch((err) => {
+          console.log(err.message)
         });
     }
     fetchData();
   }, [lat, long])
 
-  // console.log(`${process.env.REACT_APP}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+
   return (
     <div className="App">
 
       {(typeof data.main != 'undefined') ?
         <Weather weatherData={data} />
         :
-        <div></div>
+        <div>Hello</div>
       }
     </div>
   );
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,21 +1,33 @@
 import { Card } from 'semantic-ui-react'
-export default function WeatherCard({weatherData}) {
+export default function WeatherCard({ weatherData }) {
   const feelsLike = Math.round(weatherData.main.feels_like);
   const temp = Math.round(weatherData.main.temp);
   const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString();
   const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString();
-  const clouds = weatherData.weather[0].description
-  console.log(weatherData)
+  const clouds = weatherData.weather[0].description.toUpperCase()
+  // console.log(weatherData)
   return (
-    <Card>
-      <Card.Content header={weatherData.name}/>
-        <ul>
-          <li>Current Temp: {temp}ºF</li>
-          <li>Feels Like: {feelsLike}ºF</li>
-          <li>Sunrise:{sunrise} </li>
-          <li>Sunset: {sunset} </li>
-          <li>{clouds}</li>
-        </ul>
-    </Card>
+    <>
+      <div className='weatherBox'>
+        <div className='box1'>
+          <h1>{weatherData.name}</h1>
+        </div>
+        <div className='box2'>
+          <h4>Temp:<br/>&nbsp;{temp}ºF</h4>
+        </div>
+        <div className='box3'>
+          <h4>Feels Like:<br/>&nbsp;{feelsLike}ºF</h4>
+        </div>
+        <div className='box4'>
+          <h4>Sunrise:<br/>&nbsp;{sunrise}</h4>
+        </div>
+        <div className='box5'>
+          <h4>Sunset:<br/>&nbsp;{sunset}</h4>
+        </div>
+        <div className='box6'>
+          <h4>Sky:&nbsp;{clouds}</h4>
+        </div>
+      </div>
+    </>
   )
 }
