7b23135a6d049874f056b4917438e37f11dcab37
brennanp93 <brennan.p.perez@gmail.com>
2023-02-27 20:49:06 -0800

    update to remove seconds from sunrise/set

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -3,10 +3,10 @@ import moment from 'moment';
 export default function WeatherCard({ weatherData }) {
   const feelsLike = Math.round(weatherData.main.feels_like);
   const temp = Math.round(weatherData.main.temp);
-  const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString();
-  const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString();
+  const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});;
   const clouds = weatherData.weather[0].description.toUpperCase()
-  // console.log(weatherData)
+  console.log(weatherData)
   return (
     <>
           <div className='weatherBox'>
