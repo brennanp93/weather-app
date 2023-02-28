b68b8abb23f2cb45e4885318d78e29dfea301741
brennanp93 <brennan.p.perez@gmail.com>
2023-02-27 21:11:23 -0800

    added icon

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -3,34 +3,39 @@ import moment from 'moment';
 export default function WeatherCard({ weatherData }) {
   const feelsLike = Math.round(weatherData.main.feels_like);
   const temp = Math.round(weatherData.main.temp);
-  const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
-  const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});;
+  const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });;
   const clouds = weatherData.weather[0].description.toUpperCase()
-  console.log(weatherData)
+  const weatherIcon = weatherData.weather[0].icon
+  console.log(`${process.env.REACT_APP_ICON_URL}n/${weatherIcon}.png`)
   return (
     <>
-          <div className='weatherBox'>
-            <div className='box1'>
-              <h1>{weatherData.name}</h1>
-              <p>{moment().format('dddd')},&nbsp;
-                {moment().format('MMMM Do YYYY, h:mm a')}</p>
-            </div>
-            <div className='box2'>
-              <h4>Temp:<br />&nbsp;{temp}ºF</h4>
-            </div>
-            <div className='box3'>
-              <h4>Feels Like:<br />&nbsp;{feelsLike}ºF</h4>
-            </div>
-            <div className='box4'>
-              <h4>Sunrise:<br />&nbsp;{sunrise}</h4>
-            </div>
-            <div className='box5'>
-              <h4>Sunset:<br />&nbsp;{sunset}</h4>
-            </div>
-            <div className='box6'>
-              <h4>Sky:&nbsp;{clouds}</h4>
-            </div>
+      <div className='weatherBox'>
+        <div className='box1'>
+          <h1>{weatherData.name}</h1>
+          <p>{moment().format('dddd')},&nbsp;
+            {moment().format('MMMM Do YYYY, h:mm a')}</p>
+        </div>
+        <div className='box2'>
+          <h4>Temp:<br />&nbsp;{temp}ºF</h4>
+        </div>
+        <div className='box3'>
+          <h4>Feels Like:<br />&nbsp;{feelsLike}ºF</h4>
+        </div>
+        <div className='box4'>
+          <h4>Sunrise:<br />&nbsp;{sunrise}</h4>
+        </div>
+        <div className='box5'>
+          <h4>Sunset:<br />&nbsp;{sunset}</h4>
+        </div>
+        <div className='box6'>
+
+          <h4>{clouds}</h4>
+          <div>
+            <img src={`${process.env.REACT_APP_ICON_URL}n/${weatherIcon}.png`} alt="" />
           </div>
+        </div>
+      </div>
     </>
   )
 }
