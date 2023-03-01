087c6839ef7033a736c360b8bee7c68d8889601a
brennanp93 <brennan.p.perez@gmail.com>
2023-02-28 17:23:05 -0800

    fixed sunset/rise time format

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,8 @@
-
-export default function Weather ({weatherData}) {
+import moment from "moment";
+export default function Weather({ weatherData }) {
   const temp = Math.round(weatherData.main.temp);
-  const sunrise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  const sunset = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });;
+  let sunrise = moment(new Date(weatherData.sys.sunrise * 1000)).format('LT')
+  let sunset = moment(new Date(weatherData.sys.sunset * 1000)).format('LT')
   const clouds = weatherData.weather[0].description.toUpperCase()
   const high = Math.round(weatherData.main.temp_max)
   const low = Math.round(weatherData.main.temp_min)
@@ -11,47 +11,48 @@ export default function Weather ({weatherData}) {
   let directions = getDirections(weatherData.wind.deg)
   function getDirections(degrees) {
     const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
-    let index = Math.round(degrees/45) % 8;
+    let index = Math.round(degrees / 45) % 8;
     return directions[index]
   }
+
   return (
     <>
-    <div className='weatherBoxSmallScreen'>
-      <div className='box1'>
-        <h2>{weatherData.name}</h2>
-        <h1>{temp}ºF</h1>
-        <div>
-          <h4>{clouds}</h4>
+      <div className='weatherBoxSmallScreen'>
+        <div className='box1'>
+          <h2>{weatherData.name}</h2>
+          <h1>{temp}ºF</h1>
           <div>
-            <h5>H:{high}&nbsp;|&nbsp;L:{low}</h5>
+            <h4>{clouds}</h4>
+            <div>
+              <h5>H:{high}º&nbsp;&nbsp;|&nbsp;&nbsp;L:{low}º</h5>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='box2'>
-        <div>
-          <img src='/white-sunrise.png' alt="" />
-          <h4>{sunrise}</h4>
+        <div className='box2'>
+          <div>
+            <img src='/white-sunrise.png' alt="" />
+            <h4>{sunrise}</h4>
+          </div>
+        </div>
+        <div className='box3'>
+          <div>
+            <img src="/white-sunset.png" alt="" />
+            <h4>{sunset}</h4>
+          </div>
+        </div>
+        <div className='box4'>
+          <section>
+            <img src="/white-dew-point.png" alt="" />
+            <h4>{humidity}%</h4>
+          </section>
+        </div>
+        <div className='box5'>
+          <section>
+            <img src="/white-wind.png" alt="" />
+            <h4>{wind}mph&nbsp;{directions}</h4>
+          </section>
         </div>
       </div>
-      <div className='box3'>
-        <div>
-          <img src="/white-sunset.png" alt="" />
-          <h4>{sunset}</h4>
-        </div>
-      </div>
-      <div className='box4'>
-        <section>
-          <img src="/white-dew-point.png" alt="" />
-          <h4>{humidity}%</h4>
-        </section>
-      </div>
-      <div className='box5'>
-        <section>
-          <img src="/white-wind.png" alt="" />
-          <h4>{wind}mph&nbsp;{directions}</h4>
-        </section>
-      </div>
-    </div>
-  </>
+    </>
   )
 }
