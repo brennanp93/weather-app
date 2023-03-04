a5a01517746633f218d522c1a5489df6e61877c9
brennanp93 <brennan.p.perez@gmail.com>
2023-03-03 22:33:41 -0800

    success with 12 hour forecast

--- src/components/HourlyWeather.jsx
+++ src/components/HourlyWeather.jsx
@@ -1,33 +1,35 @@
 import moment from "moment";
 export default function HourlyWeather({ hourlyData }) {
-  const hours = ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
+  // const hours = ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
   const currentDate = new Date()
   const currentHour = parseInt(currentDate.getHours().toString().padStart(2, '0'));
+  // let newHourlyData = hourlyData
   let hourlyTemp = hourlyData?.hourly?.temperature_2m;
-
-  // let timeArray = hourlyData?.hourly?.time.map((t) => t = new Date(t *1000))
-  let timeArray = hourlyData?.hourly?.time.map((t) => t = moment(t *1000).format('h a'))
-// console.log(timeArray)
-  let shortTimeData = hourlyData?.hourly?.time.map((t) => t = new Date(t *1000).getHours())
-
+  
+  let timeArray = hourlyData?.hourly?.time.map((t) => t = moment(t * 1000).format('h a'))
+  
+  let shortTimeData = hourlyData?.hourly?.time.map((t) => t = new Date(t * 1000).getHours())
+  
   let futureIndex = shortTimeData?.indexOf(currentHour) + 12
-// console.log(typeof(currentHour))
-// console.log(shortTimeData[10], timeArray[10])
-let twelveHourForcastTemp = hourlyTemp?.slice(currentHour, futureIndex)
-let twelveHourForcastHours = timeArray?.slice(currentHour, futureIndex)
-// console.log(twelveHourForcastTemp, 'tuna')
-console.log(twelveHourForcastHours, 'tuna')
+
+
+  let twelveHourForcastTemp = hourlyTemp?.slice(currentHour, futureIndex)
+  let twelveHourForcastHours = timeArray?.slice(currentHour, futureIndex)
+
   return (
     <>
       <div className="hourlyContainer">
-        {hourlyTemp?.map((temp, idx) => (
+        {twelveHourForcastHours?.map((hours, idx) => (
           <div className="hourlyBox" key={idx}>
-            <div>{hours[idx]}</div>
+            <div>{hours}</div>
             <br></br>
-            <div className="temp">{temp}º</div>
+            < div className="temp">{twelveHourForcastTemp[idx]}º</div>
           </div>
 
         ))}
+
+ 
+
 
 
       </div>
